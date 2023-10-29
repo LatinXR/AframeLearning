35f287ca349dea7e87168c4bd0dff0cdf2972453
ThaisAlvarenga <latinxrhn@gmail.com>
2023-10-29 05:18:24 -0600

    Experimented more with the tutorial
    Added house png to do assets and experimented with animations in-built in AFrame and scripted animations

---
+++ scripts/animations.js
@@ -0,0 +1,15 @@
+// let rotationX, rotationY, rotationZ;
+
+// rotationX = rotationY = rotationZ = 0;
+
+// let rotationTotal = rotationX + " " + rotationY + " " + rotationZ;
+
+// const cuboRojo = document.getElementById("cuboRojo");
+
+// cuboRojo.setAttribute("rotation", rotationTotal);
+
+// const intervalo = setInterval(() => {
+//     rotationY++;
+//     rotationTotal = rotationX + " " + rotationY + " " + rotationZ;
+//     cuboRojo.setAttribute("rotation", rotationTotal);
+// }, 10)
