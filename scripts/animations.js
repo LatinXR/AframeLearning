857c397e964f7e551afe4d699ceb67b501af6346
ThaisAlvarenga <ata381@nyu.edu>
2023-10-31 04:25:29 -0600

    Cambien javascript para que esten animation.js y no en index.html

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,15 +1,24 @@
-// let rotationX, rotationY, rotationZ;
+// declarar variable para rotación
+let rotationY = 0;
+let rotationX = 10;
+let rotationZ = 0;
 
-// rotationX = rotationY = rotationZ = 0;
+// obtener objeto cubo rojo del html principal
+const cuboRojo = document.getElementById("cuboRojo");
 
-// let rotationTotal = rotationX + " " + rotationY + " " + rotationZ;
+/*hacer que rote usando setInterval, un método que ejecuta una función o fragmento de código de forma reiterada, con un retardo de tiempo fijo entre cada ejecución (Por ejemplo 10 en este caso).*/
+const intervalo = setInterval(() => {
+    
+    
+    //incrementar la rotación
+    rotationY++;
+    rotationX++;
+    rotationZ++;
+    //cambiar rotación del cubo rojo usando rotationY ya incrementada
+    cuboRojo.setAttribute("rotation", `${rotationX} ${rotationY*2} ${rotationZ}`);
 
-// const cuboRojo = document.getElementById("cuboRojo");
+    /* Nota: tecnicamente, en este caso las variables de rotacionY, rotacionX y rotacionZ siempre tendrían el mismo valos. 
+    En otras palabras, podriamos usar (, 'rotationY, rotationY, rotationY) en la funcion setAttribute.
+    Sin embargo, lo pongo asi para demonstración y para tener las variables por si quiero hacer cambios despues*/
 
-// cuboRojo.setAttribute("rotation", rotationTotal);
-
-// const intervalo = setInterval(() => {
-//     rotationY++;
-//     rotationTotal = rotationX + " " + rotationY + " " + rotationZ;
-//     cuboRojo.setAttribute("rotation", rotationTotal);
-// }, 10)
+}, 10);
